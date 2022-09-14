e17143f77fa55d39e00d46db113cc2badba8d6d5
Elena Savinykh <gudulina@mail.ru>
2022-09-14 21:13:34 +0700

    Add task 2

--- HomeWork3.py
+++ HomeWork3.py
@@ -4,15 +4,33 @@
 # [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 
 from random import randint
+# sp = []
+# for i in range(7):
+#     sp.append(randint(0, 10))
+# print(sp)
+
+# sum = 0
+# i = 1
+# while i < len(sp):
+#     sum += sp[i]
+#     i += 2
+# print(sum)
+
+
+# 2/ Напишите программу, которая найдёт произведение пар чисел списка.
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+# Пример:
+# [2, 3, 4, 5, 6] => [12, 15, 16];
+# [2, 3, 5, 6] => [12, 15]
 
 sp = []
-for i in range(7):
+for i in range(5):
     sp.append(randint(0, 10))
 print(sp)
 
-sum = 0
-i = 1
-while i < len(sp):
-    sum += sp[i]
-    i += 2
-print(sum)
+i = 0
+count = len(sp) - 1
+while i <= (len(sp) - 1)/2:
+    print(sp[i] * sp[count])
+    i += 1
+    count -= 1

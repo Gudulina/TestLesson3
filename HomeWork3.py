d702d805eadc778a53f47ce95b0855c49e04fec2
Elena Savinykh <gudulina@mail.ru>
2022-09-16 00:08:35 +0700

    Решаю

--- HomeWork3.py
+++ HomeWork3.py
@@ -58,13 +58,27 @@ from random import randint
 # 3 -> 11
 # 2 -> 10
 
+# num = int(input('Введите число: '))
+# sp = []
+# i = 0
+# while num >= 2:
+#     sp.append(num % 2)
+#     num = num // 2
+# sp.append(num)
+# sp.reverse()
+# print(sp)
+
+
+# 5. Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+# Пример:
+# для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+
 num = int(input('Введите число: '))
-sp = []
-i = 0
-while num >= 2:
-    sp.append(num % 2)
-    num = num // 2
-sp.append(num)
-sp.reverse()
+sp = [0, 1]
 print(sp)
 
+i = 0
+while i <= num:
+    sp.insert(2, sp[i]+sp[i+1])
+    i += 1
+print(sp)
